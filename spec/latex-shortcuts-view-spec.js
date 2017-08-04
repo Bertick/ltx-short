97cb609a8e009d5f78c8018cd348cc00b8004976
Federico <bertofederico@hotmail.it>
2017-08-04 11:59:01 +0200

    Initial release 0.1

---
+++ spec/latex-shortcuts-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LatexShortcutsView from '../lib/latex-shortcuts-view';
+
+describe('LatexShortcutsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
